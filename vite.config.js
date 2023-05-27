3f9265c8f3435fd0a04c1e03ccf5309e263ad695
Phil <19184024+pbayer8@users.noreply.github.com>
2023-05-27 11:33:00 -0600

    dist

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "/dist/",
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        screensaver: resolve(__dirname, "paths/screensaver.html"),
-        stars: resolve(__dirname, "paths/stars.html"),
-        cell: resolve(__dirname, "paths/cell.html"),
+export default defineConfig(({ command }) => {
+  return {
+    base: command === "build" ? "/symbolic-structures/dist/" : "/",
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.html"),
+          screensaver: resolve(__dirname, "paths/screensaver.html"),
+          stars: resolve(__dirname, "paths/stars.html"),
+          cell: resolve(__dirname, "paths/cell.html"),
+        },
       },
     },
-  },
+  };
 });
