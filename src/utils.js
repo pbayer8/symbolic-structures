58ade3e3a140bcdd49c5a233f01555c838e69f22
Phil <19184024+pbayer8@users.noreply.github.com>
2023-07-23 14:29:15 -0700

    url encoded life

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import qs from "qs";
+
 export const random = (min = 0, max = 1) => Math.random() * (max - min) + min;
 
 export const randomInt = (min, max) => Math.floor(random(min, max));
@@ -16,3 +18,33 @@ export const randomArray = (length, min = 0, max = 1) =>
   Array(length)
     .fill()
     .map(() => random(min, max));
+
+export const encodeObjectInUrl = (obj) => {
+  console.log("encoding params", obj);
+  const url = new URL(window.location.href);
+  const params = qs.stringify(obj, {
+    encoder: (param, defaultEncoder, charset, type) =>
+      defaultEncoder(
+        typeof param === "number" ? Math.round(param * 1000) / 1000 : param,
+        defaultEncoder,
+        charset,
+        type
+      ),
+  });
+  url.search = params;
+  window.history.replaceState({}, "", url);
+};
+
+export const decodeObjectFromUrl = () => {
+  const url = new URL(window.location.href);
+  const params = qs.parse(url.search, {
+    ignoreQueryPrefix: true,
+    decoder: (param, defaultDecoder, charset, type) => {
+      const value = defaultDecoder(param, defaultDecoder, charset, type);
+      const num = Number(value);
+      return Number.isNaN(num) ? value : num;
+    },
+  });
+  console.log("decoded params", params);
+  return params;
+};
